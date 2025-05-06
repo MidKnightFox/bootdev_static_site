da077264f5c2f65f9ca30af89447cce4154fa500
MidKnightFox <mail@davidvasica.net>
2025-05-07 00:02:50 +1000

    CH2L1 Submission

---
+++ src/main.py
@@ -0,0 +1,9 @@
+from textnode import *
+
+print("hello world")
+
+def main():
+    test = TextNode("This is some anchor text", TextType("normal"), "https://www.boot.dev")
+    print(test)
+
+main ()
