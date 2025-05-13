062470811f74b41d6dd865a4c63441bf2627ec9d
MidKnightFox <mail@davidvasica.net>
2025-05-13 23:32:51 +1000

    CH2L3  Submission

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,4 +1,4 @@
-class HTMLNODE:
+class HTMLNode:
     def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag
         self.value = value
@@ -8,16 +8,18 @@ class HTMLNODE:
     def to_html(self):
         raise NotImplementedError
 
-    # In progress
     def props_to_html(self):
         html = ""
-        keys = list(self.props)
-        if len(keys) == 0:
+        if self.props is not None and not isinstance(self.props, dict):
+            raise TypeError("Incorrect prop type. Please ensure type is a Dictionary")
+
+        if self.props == None or len(self.props) == 0:
             return ""
-            
-        for key in keys:
-            html += (self.props[key])            
-        return html
+
+        else:
+            for key, value in self.props.items():
+                html += F" {key}: {value}"
+            return html
     
     def __repr__(self):
-        return f"HTMLNODE({self.tag}, {self.value}, {self.children}, {self.props})"
+        return f"HTMLNode({self.tag}, {self.value}, {self.children}, {self.props})"
