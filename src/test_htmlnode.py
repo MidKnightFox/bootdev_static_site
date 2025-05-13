13260e91c74bffab9bacf0b91a40cf69b787a615
MidKnightFox <mail@davidvasica.net>
2025-05-13 17:20:29 +1000

    updated unit tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,10 +1,36 @@
 import unittest
 from htmlnode import *
 
-class HTMLNODE(unittest.TestCase):
+class HTMLNode_test(unittest.TestCase):
     def test_props_html(self): # Test prop to html conversion
-        node = HTMLNODE(props={"href": "https://www.google.com","target": "_blank",})
-        self.assertEqual(HTMLNODE.props(), "href: https://www.google.com","target: _blank")
+        node = HTMLNode(props={"href": "https://www.google.com","target": "_blank",})
+        expected = " href: https://www.google.com target: _blank"
+        result = self.assertEqual(node.props_to_html(), expected)
+        print(f"Test normal prop input")
+        print(f"Input: {node}")
+        print(f"Expected Result: {expected}")
+        print(f"Actual Result: {node.props_to_html()}")
+        print("")
+
+    def test_props_none_type(self):
+        node = HTMLNode(props=None)
+        expected = ""
+        self.assertEqual(node.props_to_html(), expected)
+        print(f"Test prop value is None")
+        print(f"Input: {node}")
+        print(f"Expected Result: {expected}")
+        print(f"Actual Result: {node.props_to_html()}")
+        print("")
+    
+    def test_props_invalid_prop_type(self):
+        node = HTMLNode(props=" href: https://www.google.com target: _blank")
+        expected = "Incorrect prop type. Please ensure type is a Dictionary"
+        self.assertRaises(node.props_to_html(), expected)
+        print(f"Test prop value is None")
+        print(f"Input: {node}")
+        print(f"Expected Result: {expected}")
+        print(f"Actual Result: {node.props_to_html()}")
+        print("")
 
 if __name__ == "__main__":
     unittest.main()
